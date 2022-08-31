d1472bcfb70937da44c266411acecc6edae0ec3c
wxh <1337865173@qq.com>
2022-08-31 10:02:55 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,10 @@ const { title } = require('process');
 module.exports = {
   mode: "development", // "production" | "development" | "none"
   // Chosen mode tells webpack to use its built-in optimizations accordingly.
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
   entry: "./src/index.js", // string | object | array
   // defaults to ./src
   // Here the application starts executing
